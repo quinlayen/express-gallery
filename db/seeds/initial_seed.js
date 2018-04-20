5c5914cd1ed20f9ed35f8822b4a0ca38bc2574af
quinlayen <quinlayen@gmail.com>
2018-04-19 17:32:30 -1000

    get by id and post working

--- db/seeds/initial_seed.js
+++ db/seeds/initial_seed.js
@@ -6,9 +6,10 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('photo').insert([
-        { author: 'Meow', link: 'http://www.meowhole.com/wp-content/uploads/2016/10/Cat-Meme-Ladies.jpg', description: 'This cat has a "HUGE" tail'},
-        { author: 'Alkeida', link: 'http://www.engrish.com/wp-content/uploads//2018/03/bomb-car-rent.jpg', description: 'Guaranteed to last at least 2 times'},
-        { author: 'Mr. Turtle', link: 'http://crazyhyena.com/imagebank/g/faster-dude-trying-birds-fly-photo.jpg', description: 'slowpoke'}
+        { author: 'Unknown', link: 'https://www.scubadiving.com/sites/scubadiving.com/files/styles/1000_1x_/public/import/2013/files/_images/201304/caves6.jpg?itok=dwi60nE7', description: 'Ordo Cave, Russia'},
+        { author: 'Expert Vagabond', link: 'https://photos.smugmug.com/Travel/Mexico/i-HjnsLdr/1/XL/mexico-cavern-diving-dos-ojos-XL.jpg', description: 'Dos Ojos Cave, Yukatan, Mexico'},
+        { author: 'Barcroft Media', link: 'https://us-east.manta.joyent.com/condenast/public/cnt-services/production/2014/07/31/53da9eeadcd5888e145c1014_piccaninnie-ponds-australia.jpg', description: 'Piccaninnie Ponds, Australia'},
+        { author: 'Mark Long', link: 'http://belizedivingservices.net/wp-content/uploads/2015/09/Giant-Cave-Caye-Caulker-Belize-3-1024x502.jpg', description: 'Giant Cave, Belize'}
       ]);
     });
 };
--- routes/photosRoute.js
+++ routes/photosRoute.js
@@ -2,101 +2,108 @@ const express = require('express');
 const router = express.Router();
 const Photos = require('../db/models/Photos');
 
-
 // get a list of all photos
-router.get('/', (req,res)=>{
-    Photos 
-    .fetchAll()
-    .then( data=>{
-        const dataObj = {photos:data.toJSON()}
-        const photos = dataObj.photos;
-        console.log('data', dataObj.photos);
-        res.render('templates/index.hbs', {
-            pageTitle: 'Photo Gallery',
-            photos
-        })
-    })
-    .catch(err=>{
-        res.json(err)
-    })
-})
+router.get('/', (req, res) => {
+  Photos.fetchAll()
+    .then(data => {
+      const dataObj = { photos: data.toJSON() };
+      const photos = dataObj.photos;
 
-// get photo by photo_id
-router.get('/gallery/:id', (req,res)=>{
-    const { id } = req.params
-    Photos
-    .where({photo_id: id})
-    .fetchAll()
-    .then(data=>{
-        res.json(data.serialize())
+      res.render('templates/index.hbs', {
+        pageTitle: 'Photo Gallery',
+        photos
+      });
     })
-    .catch(err=>{
-        res.json(err)
-    })
-})
+    .catch(err => {
+      res.json(err);
+    });
+});
 
 // show form field to create a new photo
-router.get('/gallery/new', (req,res)=>{
-    
-})
-
-// create a new photo
-router.post('/gallery', (req,res)=>{
-    const payload ={
-            author: req.body.author,
-            link: req.body.link,
-            description: req.body.description
-    }
-    Photos
-    .forge(payload)
-    .save()
-    .then(data=>{
-        res.json(data)
-    })
-    .catch(err=>{
-        res.json(err)
-    })
-
-})
+router.get('/gallery/new', (req, res) => {
+  res.render('templates/new.hbs', {
+    pageTitle: 'Enter a new photo'
+  })
+  .catch(err=>{
+      res.json(err);
+  })
+});
 
 // show a form field to edit an existing photo
-router.get('/gallery/:id/edit', (req,res)=>{
-    res.send('sanity check')
-})
+router.get('/gallery/:id/edit', (req, res) => {
+  res.render('templates/edit.hbs',{
+      pagetitle: 'Edit an photo'
+  });
+});
+
+// get photo by photo_id
+router.get('/gallery/:id', (req, res) => {
+  const { id } = req.params;
+  Photos.where({ photo_id: id })
+    .fetchAll()
+    .then(data => {
+      const dataObj = { photos: data.toJSON() };
+      const photos = dataObj.photos[0];
+
+      res.render('templates/individualPhoto.hbs', {
+        pageTitle: photos.description,
+        link: photos.link,
+        author: photos.author
+      });
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+// create a new photo
+router.post('/gallery', (req, res) => {
+  const payload = {
+    author: req.body.author,
+    link: req.body.link,
+    description: req.body.description
+  };
+  Photos.forge(payload)
+    .save()
+    .then(data => {
+      res.redirect('/');
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
 // edit an existing photo
-router.put('/gallery/:id', (req,res)=>{
-    const { id } = req.params;
-    const payload ={
-        author: req.body.author
-    }
-    Photos
-    .where({photo_id: id})
+router.put('/gallery/:id', (req, res) => {
+  const { id } = req.params;
+  const payload = {
+    author: req.body.author
+  };
+  Photos.where({ photo_id: id })
     .fetch()
-    .then(data=>{
-        return data.save(payload)
+    .then(data => {
+      return data.save(payload);
     })
-    .then(data=>{
-        res.json(data)
+    .then(data => {
+      res.json(data);
     })
-    .catch(err=>{
-        res.json(err)
-    })
-})
+    .catch(err => {
+      res.json(err);
+    });
+});
 
 // delete an exisiting photo
-router.delete('/gallery/:id', (req,res)=>{
-    const { id } = req.params;
+router.delete('/gallery/:id', (req, res) => {
+  const { id } = req.params;
 
-    Photos
-    .where({photo_id:id})
+  Photos.where({ photo_id: id })
     .destroy()
-    .then(data=>{
-        res.json(data)
+    .then(data => {
+      res.json(data);
     })
-    .catch(err=>{
-        res.json(err)
-    })
-})
+    .catch(err => {
+      res.json(err);
+    });
+});
 
 module.exports = router;
